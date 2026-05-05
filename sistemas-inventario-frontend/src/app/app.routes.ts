6d43d65e661a2d432d9546211fe7b44e1be4e0aa
SSEBASTHIAN <ssebasthian.perez@gmail.com>
2026-05-05 10:58:02 -0500

    Se Enruta Paginas Registros (Empleado, Equipo, Telefono, Tablet, Impresora) FRONT END

--- sistemas-inventario-frontend/src/app/app.routes.ts
+++ sistemas-inventario-frontend/src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { AccesoComponent } from './pagina/autenticacion/acceso/acceso.component'
 import { PerfilComponent } from './pagina/autenticacion/perfil/perfil.component';
 import { estadoPrivado, estadoPublico } from './arquitectura/guardianRuta/enturamiento.guard';
 import { OpcionesComponent } from './pagina/registro/opciones/opciones.component';
+import { OpcEmpleadoComponent } from './pagina/registro/opc-empleado/opc-empleado.component';
+import { OpcEquipoComponent } from './pagina/registro/opc-equipo/opc-equipo.component';
+import { OpcTelefonoComponent } from './pagina/registro/opc-telefono/opc-telefono.component';
+import { OpcTabletComponent } from './pagina/registro/opc-tablet/opc-tablet.component';
+import { OpcImpresoraComponent } from './pagina/registro/opc-impresora/opc-impresora.component';
 
 
 export const routes: Routes = [
@@ -32,6 +37,31 @@ export const routes: Routes = [
             {
                 path: 'opciones',
                 component: OpcionesComponent,
+                canActivate: [estadoPrivado],
+            },
+            {
+                path: 'opciones',
+                component: OpcEmpleadoComponent,
+                canActivate: [estadoPrivado],
+            },
+            {
+                path: 'opciones',
+                component: OpcEquipoComponent,
+                canActivate: [estadoPrivado],
+            },
+            {
+                path: 'opciones',
+                component: OpcTelefonoComponent,
+                canActivate: [estadoPrivado],
+            },
+            {
+                path: 'opciones',
+                component: OpcTabletComponent,
+                canActivate: [estadoPrivado],
+            },
+            {
+                path: 'opciones',
+                component: OpcImpresoraComponent,
                 canActivate: [estadoPrivado],
             },
         ],
--- sistemas-inventario-frontend/src/app/pagina/registro/opciones/opciones.component.ts
+++ sistemas-inventario-frontend/src/app/pagina/registro/opciones/opciones.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { OpcEmpleadoComponent } from '../opc-empleado/opc-empleado.component';
+import { OpcEquipoComponent } from '../opc-equipo/opc-equipo.component';
+import { OpcTelefonoComponent } from '../opc-telefono/opc-telefono.component';
+import { OpcTabletComponent } from '../opc-tablet/opc-tablet.component';
+import { OpcImpresoraComponent } from '../opc-impresora/opc-impresora.component';
+
 
 @Component({
   selector: 'app-opciones',
-  imports: [MatIconModule],
+  imports: [MatIconModule, CommonModule, OpcEmpleadoComponent, OpcEquipoComponent, OpcTelefonoComponent, OpcTabletComponent, OpcImpresoraComponent],
   templateUrl: './opciones.component.html',
   styleUrl: './opciones.component.css'
 })
 export class OpcionesComponent {
+
+  componenteActivo: string = '';
+
+
+  // Método para alternar la visibilidad del componente de registro
+  mostrarComponenteRegistroEmpleado() {
+    this.componenteActivo = this.componenteActivo === 'usuario' ? '' : 'usuario';
+  }
+
+
+  // Método para alternar la visibilidad del componente de registro
+  mostrarComponenteRegistroEquipo() {
+    this.componenteActivo = this.componenteActivo === 'equipo' ? '' : 'equipo';
+  }
+
+
+  // Método para alternar la visibilidad del componente de registro
+  mostrarComponenteRegistroCelular() {
+    this.componenteActivo = this.componenteActivo === 'celular' ? '' : 'celular';
+  }
+
+
+  // Método para alternar la visibilidad del componente de registro
+  mostrarComponenteRegistroTablet() {
+    this.componenteActivo = this.componenteActivo === 'tablet' ? '' : 'tablet';
+  }
+
+
+  // Método para alternar la visibilidad del componente de registro
+  mostrarComponenteRegistroImpresora() {
+    this.componenteActivo = this.componenteActivo === 'impresora' ? '' : 'impresora';
+  }
+
 
 }
