ce5de6365fcf349f6d3cef9899c28109002db474
SSEBASTHIAN <ssebasthian.perez@gmail.com>
2026-05-05 13:50:20 -0500

    Se Crea Interface TELEFONO - FRONT END

---
+++ sistemas-inventario-frontend/src/app/arquitectura/interface/Registro/opc-telefono.interface.ts
@@ -0,0 +1,18 @@
+export default interface telefono{
+    serial: string;
+    imgTelefono: string;
+    tipoTelefono: string;
+    marca: string;
+    modelo: string;
+    imei1: string;
+    imei2: string;
+    ram: string;
+    espacio: string;
+    versionAndroid: string;
+    cpu: string;
+    plaqueta: string;
+    facturaCompra: string;
+    fechaCompra: Date;
+    estado: string;
+    descripcion: string;
+}
